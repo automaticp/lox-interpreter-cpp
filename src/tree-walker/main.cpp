9e53f341f0f06010c38e0dfd0e084bb09dc9c9ab
automaticp <automaticpte@gmail.com>
2022-10-06 21:33:30 +0300

    implement run_file() and run_prompt()

--- src/tree-walker/main.cpp
+++ src/tree-walker/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <stdexcept>
 #include <cxxopts.hpp>
 
 
-void run_file(const std::string& file) {}
-void run_prompt() {}
+void run(const std::string& text) {}
+
+std::string read_file(const std::string& file) {
+
+    std::ifstream fs{ file };
+
+    if (!fs.fail()) {
+        return {
+            std::istreambuf_iterator<char>(fs),
+            std::istreambuf_iterator<char>()
+        };
+    } else {
+        throw std::runtime_error(
+            "Cannot open source file: " + file
+        );
+    }
+}
+
+void run_file(const std::string& file) {
+    run(read_file(file));
+}
+
+void run_prompt() {
+
+    std::string line{};
+    std::cout << "Starting lox interpreter prompt...\n";
+    std::cout << "> ";
+    while (std::getline(std::cin, line)) {
+        run(line);
+        std::cout << "> ";
+    }
+    std::cout << std::endl;
+
+}
+
 
 
 int main(int argc, char* argv[]) {
