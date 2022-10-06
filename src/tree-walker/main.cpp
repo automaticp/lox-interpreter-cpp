1177846903fd0a9690e54fd26862fcb9253ee222
automaticp <automaticpte@gmail.com>
2022-10-06 20:51:27 +0300

    add basic cli options support

--- src/tree-walker/main.cpp
+++ src/tree-walker/main.cpp
@@ -1,8 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cxxopts.hpp>
 
 
+void run_file(const std::string& file) {}
+void run_prompt() {}
 
 
 int main(int argc, char* argv[]) {
+
+    cxxopts::Options opts{ "lox-twi", "lox tree-walk interpreter" };
+    opts.add_options()
+        ("h,help", "Show help and exit")
+        ("input", "Input file to be parsed", cxxopts::value<std::string>());
+    opts.parse_positional("input");
+    opts.positional_help("file");
+
+    auto optres = opts.parse(argc, argv);
+
+    if (optres.count("help")) {
+        std::cout << opts.help() << '\n';
+        return 0;
+    }
+
+    if (optres.count("input")) {
+        run_file(optres["input"].as<std::string>());
+    } else {
+        run_prompt();
+    }
 
     return 0;
 }
