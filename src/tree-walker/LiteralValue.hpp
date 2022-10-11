6b0ee3196efec9ba638bbde1a892a0d29ddc0d14
automaticp <automaticpte@gmail.com>
2022-10-12 00:23:18 +0300

    extend literals to support bool and nil types

--- src/tree-walker/LiteralValue.hpp
+++ src/tree-walker/LiteralValue.hpp
@@ -2,9 +2,11 @@
 #include <variant>
 #include <string>
 
-using LiteralValue = std::variant<std::string, double>;
+using LiteralValue = std::variant<std::string, double, bool, nullptr_t>;
 
 struct LiteralToStringVisitor {
     std::string operator()(const std::string& val) { return val; }
     std::string operator()(const double& val) { return std::to_string(val); }
+    std::string operator()(const bool& val) { return { val ? "true" : "false" }; }
+    std::string operator()(const nullptr_t& val) { return { "nil" }; }
 };
