c61e9020a0717a72cb7328d8b52a2799ddf36c3b
automaticp <automaticpte@gmail.com>
2022-10-12 13:21:34 +0300

    comment on implementation details

--- src/tree-walker/Visitable.hpp
+++ src/tree-walker/Visitable.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 
-// If inherited from, the following classes
-// add accept() overloads
+// If inherited from, the following classes add accept() overloads
 // for all the visitors listed in ...Visitors
 // with the correct return types
 
@@ -61,4 +60,37 @@ struct Visitable<CRTP, LastVisitor> {
     friend LastVisitor;
 
 };
+
+
+// Implementation FAQ
+//
+// Q: Why not have some IVisitor interface class and define only one
+//
+//        return_type accept(const IVisitor&) const;
+//
+//    method?
+//
+// A: Makes it impossible to have different return types for different visitors,
+//    as the return type will be inquired from the parameter type (IVisitor).
+//    Simulating dynamic return types will require even more work.
+//    Retured value instead could be stored inside the Visitor, but that also
+//    entails annoyance of unpacking and mutability requirement for the Visitor.
+//
+// Q: Why not just define a single templated method
+//
+//        template<typename V>
+//        auto accept(const V& visitor) const {
+//            return visitor(static_cast<const CRTP&>(*this));
+//        }
+//
+//    in the Visitable base class instead of unpacking
+//    an explicit list of visitor types?
+//
+// A: The template instantiations between the interface Expr class
+//    and its concrete derived classes will not be syncronized.
+//    Instantiations will only happen when they are invoked somewhere.
+//    In practice, all the invokations happen through the base class pointer,
+//    which means that the accept() methods will only be instantiated for
+//    the base class, leaving the derived classes without the required overrides.
+
 
