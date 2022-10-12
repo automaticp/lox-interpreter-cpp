57953c9bcaebe998e611208875feaf161bfd358c
automaticp <automaticpte@gmail.com>
2022-10-12 10:10:07 +0300

    fix nullptr_t and non-inline free function

--- src/tree-walker/LiteralValue.hpp
+++ src/tree-walker/LiteralValue.hpp
@@ -1,16 +1,17 @@
 #pragma once
 #include <variant>
 #include <string>
+#include <cstddef>
 
-using LiteralValue = std::variant<std::string, double, bool, nullptr_t>;
+using LiteralValue = std::variant<std::string, double, bool, std::nullptr_t>;
 
 struct LiteralToStringVisitor {
     std::string operator()(const std::string& val) { return val; }
     std::string operator()(const double& val) { return std::to_string(val); }
     std::string operator()(const bool& val) { return { val ? "true" : "false" }; }
-    std::string operator()(const nullptr_t& val) { return { "nil" }; }
+    std::string operator()(const std::nullptr_t& val) { return { "nil" }; }
 };
 
-std::string to_string(const LiteralValue& literal) {
+inline std::string to_string(const LiteralValue& literal) {
     return std::visit(LiteralToStringVisitor{}, literal);
 }
