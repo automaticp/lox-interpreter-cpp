b36dcaa316e6febe631bed7b2298c2afe2a04d66
automaticp <automaticpte@gmail.com>
2022-10-12 21:53:15 +0300

    quote string literals in to_string()

--- src/tree-walker/LiteralValue.hpp
+++ src/tree-walker/LiteralValue.hpp
@@ -6,7 +6,7 @@
 using LiteralValue = std::variant<std::string, double, bool, std::nullptr_t>;
 
 struct LiteralToStringVisitor {
-    std::string operator()(const std::string& val) { return val; }
+    std::string operator()(const std::string& val) { return '"' + val + '"'; }
     std::string operator()(const double& val) { return std::to_string(val); }
     std::string operator()(const bool& val) { return { val ? "true" : "false" }; }
     std::string operator()(const std::nullptr_t& val) { return { "nil" }; }
