eadb643828ab378835445467c9fbd70847492128
automaticp <automaticpte@gmail.com>
2022-10-13 09:24:05 +0300

    add Scanner test

--- tests/tree-walker/Scanner.test.cpp
+++ tests/tree-walker/Scanner.test.cpp
@@ -39,4 +39,46 @@ TEST_CASE("init-expressions") {
 
 }
 
+TEST_CASE("multiline") {
+
+    StreamErrorReporter err{ std::cerr };
+
+    SUBCASE("if-statement-with-comments") {
+
+        Scanner s{
+            "// super algorithm\n"
+            "if (a > 5) {\n"
+            "  /* reduce /***/ */\n"
+            "  a = a / 5;\n"
+            "}",
+            err
+        };
+
+        const auto& tokens = s.scan_tokens();
+
+        REQUIRE(tokens.size() == 14);
+
+        CHECK(tokens[0] == Token{ TokenType::kw_if, "if", 2 });
+        CHECK(tokens[1] == Token{ TokenType::lparen, "(", 2 });
+        CHECK(tokens[2] == Token{ TokenType::identifier, "a", 2 });
+        CHECK(tokens[3] == Token{ TokenType::greater, ">", 2 });
+        CHECK(tokens[4] == Token{ TokenType::number, "5", 2 });
+        CHECK(tokens[5] == Token{ TokenType::rparen, ")", 2 });
+        CHECK(tokens[6] == Token{ TokenType::lbrace, "{", 2 });
+        CHECK(tokens[7] == Token{ TokenType::identifier, "a", 4 });
+        CHECK(tokens[8] == Token{ TokenType::eq, "=", 4 });
+        CHECK(tokens[9] == Token{ TokenType::identifier, "a", 4 });
+        CHECK(tokens[10] == Token{ TokenType::slash, "/", 4 });
+        CHECK(tokens[11] == Token{ TokenType::number, "5", 4 });
+        CHECK(tokens[12] == Token{ TokenType::semicolon, ";", 4 });
+        CHECK(tokens[13] == Token{ TokenType::rbrace, "}", 5 });
+
+    }
+
+
+
+}
+
+
+
 }
