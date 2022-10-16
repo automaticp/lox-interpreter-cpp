4dbde6f98059e08b3908690493ed17421e2dbc51
automaticp <automaticpte@gmail.com>
2022-10-16 18:41:31 +0300

    fix constness of literal to_string visitor methods

--- src/tree-walker/LiteralValue.hpp
+++ src/tree-walker/LiteralValue.hpp
@@ -6,10 +6,10 @@
 using LiteralValue = std::variant<std::string, double, bool, std::nullptr_t>;
 
 struct LiteralToStringVisitor {
-    std::string operator()(const std::string& val) { return '"' + val + '"'; }
-    std::string operator()(const double& val) { return std::to_string(val); }
-    std::string operator()(const bool& val) { return { val ? "true" : "false" }; }
-    std::string operator()(const std::nullptr_t& val) { return { "nil" }; }
+    std::string operator()(const std::string& val) const { return '"' + val + '"'; }
+    std::string operator()(const double& val) const { return std::to_string(val); }
+    std::string operator()(const bool& val) const { return { val ? "true" : "false" }; }
+    std::string operator()(const std::nullptr_t& val) const { return { "nil" }; }
 };
 
 inline std::string to_string(const LiteralValue& literal) {
