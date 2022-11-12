d13b26104ceb943293878d2a63740e442ff137d8
automaticp <automaticpte@gmail.com>
2022-11-12 19:46:33 +0300

    add basic Disassembler

---
+++ src/bytecode-vm/Disassembler.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "Chunk.hpp"
+#include <fmt/core.h>
+#include <string>
+#include <utility>
+
+
+class Disassembler {
+private:
+    std::string repr_;
+
+public:
+    [[nodiscard]]
+    std::string disassemble(const std::string& name, const Chunk& chungus) {
+        clear();
+        add_chunk_label(name);
+        for (size_t offset{ 0 }; offset < chungus.bytes().size(); /*_*/) {
+            offset = disassemble_instruction(chungus.bytes()[offset], offset);
+        }
+        return std::move(repr_);
+    }
+
+private:
+    size_t disassemble_instruction(Byte byte, size_t offset) {
+        switch(OP{ byte }) {
+            case OP::RETURN:
+                add_op_line("RETURN", offset);
+                ++offset;
+                break;
+            default:
+                add_op_line("UNKNOWN", offset);
+                ++offset;
+                break;
+        }
+        return offset;
+    }
+
+    void add_chunk_label(const std::string& label) {
+        add_line(fmt::format(".{:s}:", label));
+    }
+
+    void add_op_line(const std::string& line, size_t offset) {
+        add_line(fmt::format("{:04d} {}", offset, line));
+    }
+
+    void add_line(const std::string& line) {
+        repr_ += line + '\n';
+    }
+
+    void clear() {
+        repr_.clear();
+    }
+
+};
--- src/bytecode-vm/main.cpp
+++ src/bytecode-vm/main.cpp
@@ -1,6 +1,15 @@
-
-
+#include "Chunk.hpp"
+#include "Disassembler.hpp"
+#include <iostream>
 
 int main(int argc, const char* argv[]) {
+
+    Chunk chunk;
+    chunk.emit(OP::RETURN);
+    chunk.emit(OP{ 12 });
+
+    Disassembler diss;
+
+    std::cout << diss.disassemble("This Chungus", chunk);
 
 }
